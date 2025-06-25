126ba0b8dd14c06a289d40b4840429ab61cc73e1
Anailson-XD <anailson.lima33@aluno.ifce.edu.br>
2025-06-25 15:24:52 -0300

    Update utilitarios.py
    funções uteis que podem ser repetidas

--- ibex/utilitarios.py
+++ ibex/utilitarios.py
@@ -1,9 +1,22 @@
+import os
+import sqlite3
+import time
+import requests
+
+empresa_logada = None
+cliente_logado = None
+favoritos = {}
+
 def exibir_nome():
     print("#################################")
     print("Ｂｅｍ ｖｉｎｄｏ ａｏ Ｉｂｅｘ！")
     print("#################################")
 
 def exibir_subtitulo(texto):
-    print("\n" + "#" * 40)
-    print(f"{texto}")
-    print("#" * 40)
+    os.system('cls')
+    exibir_nome()
+    print(texto)
+    print()
+
+def finalizando_app():
+    exibir_subtitulo('Finalizando App')
