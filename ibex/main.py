64b383ca676eebcee098a9b837bf941bc0d4a8e9
Anailson-XD <anailson.lima33@aluno.ifce.edu.br>
2025-06-25 15:16:54 -0300

    Update main.py

--- ibex/main.py
+++ ibex/main.py
@@ -1,5 +1,11 @@
+from ibex.utilitarios import exibir_nome
+from ibex.menus import menu_principal
+from ibex.banco import criar_tabelas
+
 def main():
-    print("Ibex iniciado!")
+    criar_tabelas()       # Cria tabelas do banco se não existirem
+    exibir_nome()         # Exibe o cabeçalho do sistema
+    menu_principal()      # Chama o menu principal do sistema
 
 if __name__ == '__main__':
     main()
